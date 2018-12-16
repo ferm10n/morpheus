4644a8cf3ce90633d7fe2f8595cc75d41c562745
John Sanders <jws121295@gmail.com>
2018-12-16 06:50:43 -0500

    stream to nodejs

--- Adafruit_NeoPixel_Mock.h
+++ Adafruit_NeoPixel_Mock.h
@@ -40,34 +40,49 @@ public:
     color |= b;
     return color;
   }
-  void begin () { // doesn't do much. just sets the SIGINT handler if it isn't already
-    if (!signalSet) {
+  void begin () {
+    if (!signalSet) {  // sets the SIGINT handler if it isn't already
       signalSet = true;
       signal(SIGINT, doQuit);
     }
   }
   void show () {
-    std::ostringstream os; // we build a string
-    os << '\n'; // change this to '\r' if you don't want the terminal to scroll. But this is better performance wise.
+    // std::cout << 0 << ' ' << pixels.size() << '\n';
+    putchar(0); // strip index
+    putchar(pixels.size()); // light count
+
+    // std::ostringstream os; // we build a string
+    // os << '\n'; // change this to '\r' if you don't want the terminal to scroll. But this is better performance wise.
     for (int i = 0; i < pixels.size(); i++) {
       uint32_t c = pixels.at(i);
       // bitfidly magic
       int r = (uint8_t)(c >> 16),
           g = (uint8_t)(c >> 8)&0xFF,
           b = (uint8_t)c&0xFF;
-      os << "\x1b[48;2;" << r << ";" << g << ";" << b << "m \x1b[0m";
+      // os << "\x1b[48;2;" << r << ";" << g << ";" << b << "m \x1b[0m";
+      // std::cout << r << ' ' << g << ' ' << b << ' ';
+      putchar(r);
+      putchar(g);
+      putchar(b);
+      // std::cout << 255 << 255 << '\n';
     }
-    std::string s = os.str();
-    std::cout << s;
+    // putchar('\n');
+    // std::cout << '\n';
+    // std::cout << "!\n";
+    // std::string s = os.str();
+    // std::cout << s;
+    // std::cout << 1 << std::endl;
   }
   uint16_t numPixels () {
     return pixels.size();
   }
   void setPixelColor(uint8_t i, uint32_t c) {
-    if (i < pixels.size() && i > 0) pixels.at(i) = c; // protect out of range stuff
+    if (i >= 0 && i < pixels.size()) {
+      pixels.at(i) = c; // protect out of range stuff
+    } 
   }
   static void cleanup () {
-    printf("\x1b[48;2;0;0;0m\n\x1b[0m"); // ensure color is set back to normal
+    // printf("\x1b[48;2;0;0;0m\n\x1b[0m"); // ensure color is set back to normal
   }
 };
 bool Adafruit_NeoPixel::signalSet = false;
